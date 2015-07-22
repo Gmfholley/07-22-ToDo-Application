9f79e8fa6026aafc7986a954bde8cbfd8c96ada3
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-22 15:54:28 -0500

    Adds resources and CRUD methods for users & tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  
+  # show all records
+  def index
+  end
+  
+  # receive form to create a new record
+  def create
+  end
+  
+  # look at form to create a new record
+  def new
+  end
+  
+  # show a single record
+  def show
+  end
+  
+  # see a form to update a single record
+  def edit
+  end
+  
+  # update a single record from form
+  def update
+  end
+  
+  # delete this record and all its associations
+  def destroy
+  end
+  
 end
