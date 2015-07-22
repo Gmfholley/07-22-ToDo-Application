ff4d341b4dfbe5b4e9f4b38fa2b2fad414786628
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-22 15:18:05 -0500

    Updates login to handle encryption

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,46 +1,30 @@
 module Login
-  # BCrypt
   
-  # run this to convert a password (if it is a password and not a hash) to a hash
+  # sets the @password_encrypted parameter
   #
-  # either converts the password or not if it is not a Hash
-  after_initialize do |user|
-    user.convert_password_if_necessary
-  end
-
-  # run this to convert a password (if it is a password and not a hash) to a hash
-  #
-  # either converts the password or not if it is not a Hash
-  before_update do |user|
-    user.convert_password_if_necessary
-  end
-  
-  # run this to convert a password (if it is a password and not a hash) to a hash
-  #
-  # either converts the password or not if it is not a Hash
-  def convert_password_if_necessary
-    begin
-      BCrypt::Password.new(password)
-    rescue BCrypt::Errors::InvalidHash
-      convert_password
-    end
+  # p - String of the user's typed password
+  # 
+  # returns the encrypted password as a String
+  def password=(p)
+    p_hash = encrypt_it_with_bcrypt(p)
+    @password_encrypted = p_hash
   end
   
   # encrypts the password
   #
   # returns the new password
-  def convert_password
-    self.password = BCrypt::Password.create(self.password)
+  def encrypt_it_with_bcrypt(password)
+    BCrypt::Password.create(password)
   end
   
   # checks if this is a valid password
   #
   # returns a Boolean
   def valid_password?(password)
-    if !(BCrypt::Password.new(self.password) == password)
-      errors.messages["login"] = "Login Failed."
+    if !(BCrypt::Password.new(self.password_encrypted) == password)
+      self.errors.messages["login"] = "Login Failed."
     end
-    return errors.messages.empty?
+    return self.errors.messages.empty?
   end  
   
 end
