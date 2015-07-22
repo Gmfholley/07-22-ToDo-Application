00627361eaa8a80a74dd5983e277b8aca9b2f8fb
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-22 15:49:42 -0500

    Pastes UserHelper for login in helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,48 @@
 module UsersHelper
+
+  # cache = ActiveSupport::Cache::MemoryStore.new
+  #
+  # cache.read('city')   # => nil
+  # cache.write('city', "Duckburgh")
+  # cache.read('city')   # => "Duckburgh"
+
+  def current_user
+    if session_id
+      @user = User.find(session_id)
+    else
+      clear_session
+      redirect_to "/"
+    end 
+  end 
+
+  def set_user_if_available    
+    if session_id
+      @user = User.find(session_id)
+    else
+      @user = User.new
+    end
+  end
+
+  def session_id
+    session[:id]
+  end
+
+  def clear_session
+    session[:id] = nil
+  end
+
+  def save_user_and_set_session
+    @user.save
+    set_session
+  end
+
+  def set_session
+    session[:id] = @user.id
+  end
+
+  def find_user_by_email
+    @user = User.where("email" => params["users"]["email"]).first
+  end
+
+
 end
