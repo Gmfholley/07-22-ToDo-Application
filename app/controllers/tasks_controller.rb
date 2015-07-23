164686e41c05edabb860eec29c9714b551003f54
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-23 18:06:55 -0500

    Updates routes for edit, update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,30 +2,66 @@ class TasksController < ApplicationController
   
   # show all records
   def index
+    set_user_if_available
+    set_task
   end
   
   # receive form to create a new record
   def create
+    @task = Task.new(params_permitted)
+    if @task.save
+      redirect_to my_tasks_path, :notice => "Successfully added."
+    else
+      render "new"
+    end
   end
   
   # look at form to create a new record
   def new
+    current_user
+    @task = Task.new
   end
   
   # show a single record
   def show
+    current_user
+    set_task
   end
   
   # see a form to update a single record
   def edit
+    current_user
+    set_task
   end
   
   # update a single record from form
   def update
+    current_user
+    @task = Task.find(params["task"]["id"])
+    if @task.update(params_permitted)
+      redirect_to my_tasks_path, :notice => "Successfully updated!"
+    else
+      render "edit"
+    end
   end
   
   # delete this record and all its associations
   def destroy
+    current_user
+    set_task
+  end
+  
+  private
+  def set_task
+    if params[:id].blank?
+      @task = Task.new
+    else
+      @task = Task.find(params[:id])
+    end
+  end
+  
+  def params_permitted
+    params["task"].permitted(:name, :completed)
   end
   
 end
