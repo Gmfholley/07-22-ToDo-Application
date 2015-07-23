e344765cf02b1bbacdf9a12695cef0814b8a82f9
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-23 11:50:01 -0500

    Corrects validates method name for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
-  validates :password, presence: true
+  validates :password_encrypted, presence: true
   
   
 end
