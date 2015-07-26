fc6249378d11485e4a812b1fe5095f101ffdae30
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-26 18:26:55 -0500

    Starts javascript file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,21 @@
 //= require jquery_ujs
 
 //= require_tree .
-  window.onload = function() {
-    alert("yes");
-  }
+
+// returns an Array of elements marked done
+//
+// returns an Array of Elements  
+function get_all_mark_done(){
+  return document.getElementsByClassName("mark--done");
+}
+  
+// returns an Array of elements marked undone
+//
+// returns an Array of Elements
+function get_all_mark_undone(){
+  return document.getElementsByClassName("mark--undone");
+}
+  
+window.onload = function() {
+  document.getElementsByClassName("mark")
+} 
