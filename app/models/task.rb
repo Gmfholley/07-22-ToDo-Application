59bb119262199f2217e40452da078f5711f8337d
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-26 21:39:51 -0500

    Adds undone and done methods to model

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,5 +12,26 @@ class Task < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
   
+  # updates to complete if not completed
+  #
+  # returns a Boolean if able to update
+  def mark_done
+    if !self.completed
+      self.update(completed: true)
+    else
+      false
+    end
+  end
+  
+  # updates to not complete if already completed
+  #
+  # returns a Boolean if able to update
+  def mark_undone
+    if self.completed
+      self.update(completed: false)
+    else
+      false
+    end
+  end
   
 end
